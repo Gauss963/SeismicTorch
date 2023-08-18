f8266809422ee3ca77789c9ae460c4a8b3e004ac
gauss963 <gauss963@gmail.com>
2023-08-18 11:27:30 +0800

    Include trigger

--- read_accelerometer_class.py
+++ read_accelerometer_class.py
@@ -33,10 +33,19 @@ class EarthquakeDataCollector:
         self.softmax = torch.nn.Softmax(dim = 1)
 
         self.sleep = sleep
+        self.trigger = False
 
     def onAccelerationChange(self, self_ch, acceleration, timestamp):
         acceleration_arr = np.array(acceleration) * self.g_acc # Convert to m/s2
         self.all_data.append(acceleration_arr)
+
+    def beeper_trigger(self):
+        if self.trigger == True:
+            pass
+            print('triggered')
+
+
+        self.trigger = False
 
     def start_collecting(self):
         self.ch.openWaitForAttachment(1000)
@@ -45,7 +54,7 @@ class EarthquakeDataCollector:
             while True:
                 time.sleep(self.sleep)
 
-                self.feed_data = self.process_last_2_seconds_data()
+                self.feed_data = self.get_process_last_2_seconds_data()
                 self.feed_data = self.feed_data.reshape(1, 80, 3, 1)
 
                 self.feed_data = functions.normalize(self.feed_data)
@@ -61,32 +70,45 @@ class EarthquakeDataCollector:
                 Y = Y.detach().numpy()
 
                 Y = np.round(Y * 100).astype(int)
-                print(Y)
+                # print(Y)
+                PGA = self.get_last_2_seconds_data_PGA()
+                print(PGA)
+                if PGA >= 2.0 and Y[0][2] <= 0.5:
+                    self.trigger = True
 
-        except KeyboardInterrupt:  # Allowing for a keyboard interrupt to stop the collection
+                self.beeper_trigger()
+
+        except KeyboardInterrupt:
             self.ch.close()
             print("\nData collection stopped.")
 
     def get_last_2_seconds_data(self):
         return np.array(self.all_data)
     
-    def process_last_2_seconds_data(self, alpha = 0.05, freq = 0.5):
+    def get_process_last_2_seconds_data(self, alpha = 0.05, freq = 0.5):
         data_array = self.get_last_2_seconds_data()
         stream = obspy.Stream()
         for i in range(3):
             trace = obspy.Trace(data = data_array[:, i])
+            stream.append(trace)
             trace.stats.sampling_rate = self.sampling_rate
 
-            stream.append(trace)
-            stream.detrend(type = "linear")
-            stream.taper(alpha)
-            stream.filter(type = "highpass", freq = freq)
+        stream.detrend(type = "linear")
+        stream.taper(alpha)
+        stream.filter(type = "highpass", freq = freq)
 
         feed_data = np.vstack([stream[i].data for i in range(len(stream))])
         feed_data = scipy.signal.resample(feed_data, self.dim, axis = -1)
         feed_data = feed_data.transpose()
 
         return feed_data
+    
+    def get_last_2_seconds_data_PGA(self):
+        data_array = self.get_process_last_2_seconds_data()
+        composite_acceleration = np.sqrt(data_array[:, 0]**2 + data_array[:, 1]**2 + data_array[:, 2]**2)
+        PGA = np.max(composite_acceleration) * 100 # Convert to gal
+
+        return PGA
 
 
 collector = EarthquakeDataCollector(sleep = 0.5)
