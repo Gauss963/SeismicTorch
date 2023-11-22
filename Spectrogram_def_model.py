e2348f65c06f3a7a1996f01cddc626367d15d60f
gauss963 <gauss963@gmail.com>
2023-11-22 14:55:39 +0800

    New SpectrogramCNN model

---
+++ Spectrogram_def_model.py
@@ -0,0 +1,77 @@
+import torch
+import torch.nn as nn
+from torchviz import make_dot
+from thop import profile
+
+class SpectrogramCNN(nn.Module):
+    def __init__(self):
+        super(SpectrogramCNN, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels = 3, out_channels = 48, kernel_size = (3, 3), stride = (1, 1))
+        self.bn1 = nn.BatchNorm2d(48)
+        self.GELU1 = nn.GELU()
+        self.maxpool1 = nn.MaxPool2d(kernel_size = (2, 2), stride = (2, 2))
+        self.dropout1 = nn.Dropout(p = 0.1)
+
+        self.conv2 = nn.Conv2d(in_channels = 48, out_channels = 96, kernel_size = (3, 3), stride = (1, 1))
+        self.bn2 = nn.BatchNorm2d(96)
+        self.GELU2 = nn.GELU()
+        self.maxpool2 = nn.MaxPool2d(kernel_size = (2, 2), stride = (2, 2))
+        self.dropout2 = nn.Dropout(p = 0.1)
+
+        self.conv3 = nn.Conv2d(in_channels = 96, out_channels = 192, kernel_size = (3, 3), stride = (1, 1))
+        self.bn3 = nn.BatchNorm2d(192)
+        self.GELU3 = nn.GELU()
+        self.maxpool3 = nn.MaxPool2d(kernel_size = (2, 2), stride = (2, 2))
+        self.dropout3 = nn.Dropout(p = 0.1)
+
+        self.fc1 = nn.Linear(32640, 1280)
+        self.GELU4 = nn.GELU()
+        self.fc2 = nn.Linear(1280, 64)
+        self.GELU5 = nn.GELU()
+        self.fc3 = nn.Linear(64, 2)
+        
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.GELU1(x)
+        x = self.maxpool1(x)
+        x = self.dropout1(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.GELU2(x)
+        x = self.maxpool2(x)
+        x = self.dropout2(x)
+        
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.GELU3(x)
+        x = self.maxpool3(x)
+        x = self.dropout3(x)
+
+        x = x.view(x.size(0), -1)
+
+        x = self.fc1(x)
+        x = self.GELU4(x)
+        x = self.fc2(x)
+        x = self.GELU5(x)
+        x = self.fc3(x)
+
+        return x
+
+if __name__ == "__main__":
+    model_plot = SpectrogramCNN()
+    input_tensor = torch.randn(1, 3, 100, 150)
+    output = model_plot(input_tensor)
+    # print(output)
+    print(output.shape)
+
+    total = sum([param.nelement() for param in model_plot.parameters()])
+    print("Number of parameter: %.2f" % (total))
+
+
+    macs, params = profile(model_plot, inputs = (input_tensor, ))
+    flops = macs * 2
+    print(f'The model has {flops} FLOPs')
